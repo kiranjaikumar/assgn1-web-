335afb2fd8378a19a8430503d56a18d6c496b4f3
kiranjaikumar <87845251+kiranjaikumar@users.noreply.github.com>
2022-08-03 21:46:53 +0530

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace assgn1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="assgn1.WebApiApplication" Language="C#" %>
